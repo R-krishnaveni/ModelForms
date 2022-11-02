28ddda8f7cbaef580e40830e0bf2a7c28a2effbf
krishnaveni <renatikrishnaveni2202@gmail.com>
2022-11-02 13:24:42 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from socket import fromshare
+from django import forms
+from app.models import *
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model=Topic
+        fields='__all__'
+
+class WebpageForm(forms.ModelForm):
+    class Meta:
+        model=Webpage
+        fields='__all__'
+        #fields=['topic_name','name']
+        #exclude=['url','name']
+        #help_texts={'name':'enter only alphabets'}
+        widgets={'name':forms.PasswordInput}
+class AccessRecordsForm(forms.ModelForm):
+    class Meta:
+        model=AccessRecords
+        fields='__all__'
